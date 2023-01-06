e163a6eacce98a9d29bdece26f1e989b06ab9929
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-01-06 16:54:22 +0700

    Update final 6/1/2023

--- src/gameobjects/RedEyeDevil.java
+++ src/gameobjects/RedEyeDevil.java
@@ -111,12 +111,6 @@ public class RedEyeDevil extends ParticularObject {
 						(int) ((this.getPosY() - getGameWorld().getCamera().getPosY()) - this.getHeight() / 2 - 20),
 						(int) this.getBlood(), 10);
 
-				/*
-				 * g2.setColor(Color.red); g2.fillRect((int) (this.getPosX() -
-				 * getGameWorld().getCamera().getPosX() - this.getWidth() / 2), (int)
-				 * ((this.getPosY() - getGameWorld().getCamera().getPosY()) - this.getHeight() /
-				 * 2 - 20), (int) this.getBlood(), 10);
-				 */
 				if (this.getDirection() == LEFT_DIR) {
 					backAnimation.Update(System.nanoTime());
 					backAnimation.draw(g2, (int) (this.getPosX() - getGameWorld().getCamera().getPosX()),
@@ -126,8 +120,6 @@ public class RedEyeDevil extends ParticularObject {
 					forwardAnimation.draw(g2, (int) (this.getPosX() - getGameWorld().getCamera().getPosX()),
 							(int) (this.getPosY() - getGameWorld().getCamera().getPosY()));
 				}
-
-				// test github
 			}
 		}
 	}
