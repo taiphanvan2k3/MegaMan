11afb8f546f7617eac6ab405676f92f270e642f5
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-02-05 15:48:03 +0700

    5/2/2023: test

--- src/effect/CacheDataLoader.java
+++ src/effect/CacheDataLoader.java
@@ -294,9 +294,9 @@ public class CacheDataLoader {
 					/*
 					 * Chú ý là dùng Clip chứ không dùng AudioClip nữa
 					 * Không truyền đối tượng File vào getAudioInputStream vì nếu truyền vào File là
-					 * ta đã thông qua String để tạo File. File f= new
-					 * File(getClass().getClassLoader().getResource(str[1]).getPath()); (truyền vào
-					 * String vào cho new File(String). Làm điều này chỉ chạy được trên IDE chứ
+					 * ta đã thông qua String để tạo File. 
+					 * File f= new File(getClass().getClassLoader().getResource(str[1]).getPath()); 
+					 * (truyền vào String vào cho new File(String). Làm điều này chỉ chạy được trên IDE chứ
 					 * không chạy được khi export ra jar
 					 */
 					sound = AudioSystem.getAudioInputStream(url);
